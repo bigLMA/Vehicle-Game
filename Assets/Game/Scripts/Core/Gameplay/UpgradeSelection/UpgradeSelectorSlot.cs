47fca9c34d81527fa157d7f011d6bc86ea934e5a
Michael Lashko <lashkomykhail@gmail.com>
2026-04-13 14:59:38 +0300

    Drag elements

--- Assets/Game/Scripts/Core/Gameplay/UpgradeSelection/UpgradeSelectorSlot.cs
+++ Assets/Game/Scripts/Core/Gameplay/UpgradeSelection/UpgradeSelectorSlot.cs
@@ -11,15 +11,6 @@ namespace VehicleGame.Core.Gameplay.UpgradeSelector
 
         public VehicleUpgrader _upgrader { get; private set; }
 
-        private void OnMouseDown()
-        {
-            if (_upgrader == null) return;
-
-            OnClick?.Invoke(_upgrader);
-            Destroy(_upgrader.gameObject);
-            _upgrader = null;
-        }
-
         public void InsertSlot(VehicleUpgrader upgrade)
         {
             _upgrader = Instantiate(upgrade, transform.position - new Vector3(0f,0f, 0.5f), Quaternion.Euler(0f, 90f, 0f));
--- Assets/Game/Scripts/Core/Gameplay/Vehicle/Upgrade/VehicleUpgrader/VehicleUpgrader.cs
+++ Assets/Game/Scripts/Core/Gameplay/Vehicle/Upgrade/VehicleUpgrader/VehicleUpgrader.cs
@@ -1,13 +1,35 @@
 
 using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
 
 namespace VehicleGame.Core.Gameplay.Vehicle
 {
     public abstract class VehicleUpgrader : MonoBehaviour
     {
+        private Vector3 _offset;
+
         public abstract string GetKey(); 
 
         public abstract void Upgrade(VehicleUpgradeData data);
+
+        private void OnMouseDrag()
+        {
+            var zDepth = Camera.main.WorldToScreenPoint(transform.position).z;
+
+            Vector3 touchWorld = Camera.main.ScreenToWorldPoint(
+                new Vector3(Input.mousePosition.x,
+                Input.mousePosition.y,
+                zDepth));
+
+            touchWorld.z = transform.position.z;
+
+            transform.position = touchWorld;
+        }
+
+        private void OnMouseUp()
+        {
+            
+        }
     }
 }
 
